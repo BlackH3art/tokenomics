85ecabf6831ca4ed9cb85f56c30a516a892414b1
BlackH3art <jaroslaw.musielak@outlook.com>
2023-03-25 21:39:58 +0100

    fix: valuation variants are now independent

--- client/src/components/_Reusable/Valuation/Valuation.tsx
+++ client/src/components/_Reusable/Valuation/Valuation.tsx
@@ -2,7 +2,11 @@ import { FC, ChangeEventHandler, ChangeEvent, useState, useContext } from "react
 import { PriceContext } from "../../../context/PriceContext";
 import { ValuationRow } from "../ValuationRow/ValuationRow";
 
-export const Valuation: FC = () => {
+interface Props {
+  variant: number;
+}
+
+export const Valuation: FC<Props> = ({ variant }) => {
 
   const [valuationData, setValuationData] = useState({
     capitalization: "",
@@ -35,7 +39,7 @@ export const Valuation: FC = () => {
         <div className="w-full flex flex-col md:w-4/5 xl:w-3/5 border-[1px] border-gray-500 px-10 py-10">
 
           <h1 className="text-xl font-semibold">
-            Valuation Variant 1
+            Valuation Variant {variant}
           </h1>
 
 
@@ -98,139 +102,6 @@ export const Valuation: FC = () => {
             handleChange={handleChange}
             sign="*"
           />
-
-
-          <h1 className="text-xl font-semibold pt-20">
-            Valuation Variant 2
-          </h1>
-
-
-          <ValuationRow 
-            firstLabel="Estimated valuation"
-            firstInputName="capitalization"
-            firstInputPlaceholder="capitalization ($)"
-            firstInputValue={valuationData.capitalization}
-            secondLabel="Total supply"
-            secondInputName="totalSupply"
-            secondInputPlaceholder="total supply"
-            secondInputValue={valuationData.totalSupply}
-            resultLabel="Price per token"
-            resultValue={isNaN(pricePerToken) ? "0" : `$${pricePerToken.toFixed(6)}`}
-            handleChange={handleChange}
-            sign="/"
-          />
-
-          <ValuationRow 
-            firstLabel="Private sale price"
-            firstInputName="privateSalePrice"
-            firstInputPlaceholder="private sale price ($)"
-            firstInputValue={valuationData.privateSalePrice}
-            secondLabel="Private sale allocation"
-            secondInputName="privateSaleAllocation"
-            secondInputPlaceholder="tokens amount"
-            secondInputValue={valuationData.privateSaleAllocation}
-            resultLabel="Privately raised funds"
-            resultValue={isNaN(privateRaisedFunds) ? "0" : `$${new Intl.NumberFormat('en-US', { }).format(privateRaisedFunds)} / ${(privateRaisedFunds / Number(ethereumPrice)).toFixed(4)} ETH`}
-            handleChange={handleChange}
-            sign="*"
-          />
-
-          <ValuationRow 
-            firstLabel="Public sale price"
-            firstInputName="publicSalePrice"
-            firstInputPlaceholder="public sale price ($)"
-            firstInputValue={valuationData.publicSalePrice}
-            secondLabel="Public sale allocation"
-            secondInputName="publicSaleAllocation"
-            secondInputPlaceholder="tokens amount"
-            secondInputValue={valuationData.publicSaleAllocation}
-            resultLabel="Publicly raised funds"
-            resultValue={isNaN(publiclyRaisedFunds) ? "0" : `$${new Intl.NumberFormat('en-US', { }).format(publiclyRaisedFunds)}  / ${(publiclyRaisedFunds / Number(ethereumPrice)).toFixed(4)} ETH`}
-            handleChange={handleChange}
-            sign="*"
-          />
-
-          <ValuationRow 
-            firstLabel="Liquidity pool price"
-            firstInputName="liquidityPoolPrice"
-            firstInputPlaceholder="initial price ($)"
-            firstInputValue={valuationData.liquidityPoolPrice}
-            secondLabel="Liquidity allocation"
-            secondInputName="liquidityAllocation"
-            secondInputPlaceholder="tokens amount"
-            secondInputValue={valuationData.liquidityAllocation}
-            resultLabel="Second pool token value"
-            resultValue={isNaN(liquidityValue) ? "0" : `$${new Intl.NumberFormat('en-US', { }).format(liquidityValue)} / ${(liquidityValue / Number(ethereumPrice)).toFixed(4)} ETH`}
-            handleChange={handleChange}
-            sign="*"
-          />
-
-
-          <h1 className="text-xl font-semibold pt-20">
-            Valuation Variant 3
-          </h1>
-
-
-          <ValuationRow 
-            firstLabel="Estimated valuation"
-            firstInputName="capitalization"
-            firstInputPlaceholder="capitalization ($)"
-            firstInputValue={valuationData.capitalization}
-            secondLabel="Total supply"
-            secondInputName="totalSupply"
-            secondInputPlaceholder="total supply"
-            secondInputValue={valuationData.totalSupply}
-            resultLabel="Price per token"
-            resultValue={isNaN(pricePerToken) ? "0" : `$${pricePerToken.toFixed(6)}`}
-            handleChange={handleChange}
-            sign="/"
-          />
-
-          <ValuationRow 
-            firstLabel="Private sale price"
-            firstInputName="privateSalePrice"
-            firstInputPlaceholder="private sale price ($)"
-            firstInputValue={valuationData.privateSalePrice}
-            secondLabel="Private sale allocation"
-            secondInputName="privateSaleAllocation"
-            secondInputPlaceholder="tokens amount"
-            secondInputValue={valuationData.privateSaleAllocation}
-            resultLabel="Privately raised funds"
-            resultValue={isNaN(privateRaisedFunds) ? "0" : `$${new Intl.NumberFormat('en-US', { }).format(privateRaisedFunds)} / ${(privateRaisedFunds / Number(ethereumPrice)).toFixed(4)} ETH`}
-            handleChange={handleChange}
-            sign="*"
-          />
-
-          <ValuationRow 
-            firstLabel="Public sale price"
-            firstInputName="publicSalePrice"
-            firstInputPlaceholder="public sale price ($)"
-            firstInputValue={valuationData.publicSalePrice}
-            secondLabel="Public sale allocation"
-            secondInputName="publicSaleAllocation"
-            secondInputPlaceholder="tokens amount"
-            secondInputValue={valuationData.publicSaleAllocation}
-            resultLabel="Publicly raised funds"
-            resultValue={isNaN(publiclyRaisedFunds) ? "0" : `$${new Intl.NumberFormat('en-US', { }).format(publiclyRaisedFunds)}  / ${(publiclyRaisedFunds / Number(ethereumPrice)).toFixed(4)} ETH`}
-            handleChange={handleChange}
-            sign="*"
-          />
-
-          <ValuationRow 
-            firstLabel="Liquidity pool price"
-            firstInputName="liquidityPoolPrice"
-            firstInputPlaceholder="initial price ($)"
-            firstInputValue={valuationData.liquidityPoolPrice}
-            secondLabel="Liquidity allocation"
-            secondInputName="liquidityAllocation"
-            secondInputPlaceholder="tokens amount"
-            secondInputValue={valuationData.liquidityAllocation}
-            resultLabel="Second pool token value"
-            resultValue={isNaN(liquidityValue) ? "0" : `$${new Intl.NumberFormat('en-US', { }).format(liquidityValue)} / ${(liquidityValue / Number(ethereumPrice)).toFixed(4)} ETH`}
-            handleChange={handleChange}
-            sign="*"
-          />
-
           
         </div>
       </section>
